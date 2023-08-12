85fbbb0ffa9948197f1c93a8dd048ace089e09c8
nick doyle <nick.doyle09@gmail.com>
2023-08-12 01:28:28 -0400

    Coded source_project_data.py.
    1. Every project should have a start_date, and an end_date. These should vary between 7 and 13 weeks apart per project.
    2. Every project is comprised of 5 milestones that must be completed in order. milestone 1 must start after the start_date and milestone 5 must end before the end_date.
    3. every milestone should have a due_date and a completed_date. these dates should match most of the time but sometimes should not match.
    4. every project should start on or after 1-1-2023 and every project should end on or before 12-31-2023
    5. the resulting file should contain the following columns: project_id, project_start_date, project_end_date, milestone_1_due_date, milestone_1_completed_date,...,milestone_5_due_date, milestone_5_completed_date

---
+++ fake_scratch.py
@@ -0,0 +1,11 @@
+from faker import Faker
+import random
+from datetime import datetime, timedelta
+import pandas as pd
+
+# Create a Faker instance
+fake = Faker()
+
+
+
+print(case_numbers)
---
+++ source_project_data.py
@@ -0,0 +1,69 @@
+from faker import Faker
+import pandas as pd
+import random
+from datetime import datetime, timedelta
+from source_person_data import alt_team_names
+
+# Initialize Faker and random seed
+fake = Faker()
+random.seed(42)
+
+# Define number of projects
+num=500
+
+# Generate project data
+data = []
+for project_id in range(1, num+1):
+    project_start_date = fake.date_between_dates(date_start=datetime(2023, 1, 1), date_end=datetime(2023, 12, 11))
+    project_end_date = project_start_date + timedelta(weeks=random.randint(7, 13))
+
+    for milestone in range(1, 6):
+        due_date = fake.date_between_dates(date_start=project_start_date, date_end=project_end_date)
+        completed_date = due_date + timedelta(days=random.randint(-3, 3))
+
+        data.append([project_id, project_start_date, project_end_date, milestone, due_date, completed_date])
+
+# Create DataFrame
+columns = ['project_id', 'project_start_date', 'project_end_date',
+           'milestone', 'milestone_due_date', 'milestone_completed_date']
+df_projects = pd.DataFrame(data, columns=columns)
+
+# Create a unique column name for each milestone
+df_projects['milestone'] = 'milestone_' + df_projects['milestone'].astype(str)
+projects = df_projects.set_index(['project_id', 'project_start_date', 'project_end_date', 'milestone']).unstack()
+
+# Flatten MultiIndex columns
+projects.columns = [f'{col[1]}_{col[0]}' for col in projects.columns]
+
+# Reset the index
+projects.reset_index(inplace=True)
+
+# Create a unique column name for each milestone
+df_projects['milestone'] = 'milestone_' + df_projects['milestone'].astype(str)
+projects = df_projects.set_index(['project_id',
+                           'project_start_date',
+                           'project_end_date',
+                           'milestone']).unstack()
+
+# Flatten MultiIndex columns
+projects.columns = [f'{col[1]}_{col[0]}' for col in projects.columns]
+
+# Reset the index
+projects.reset_index(inplace=True)
+
+# Replace
+case_numbers = [fake.unique.random_int(min=10000000, max=99999999) for row in range(num)]
+projects['project_id'] = case_numbers
+
+print(projects.head())
+
+# Assign a team
+projects['team_name'] = random.choices(list(alt_team_names.keys()), k=num)
+projects['alt_team_name'] = [alt_team_names[team] for team in projects['team_name']]
+
+print(projects.groupby('alt_team_name').project_id.count())
+
+# Save DataFrame to CSV
+projects.to_csv('data/project_dataset.csv', index=False)
+
+print("Dataset generated and saved as 'data/project_dataset.csv'")

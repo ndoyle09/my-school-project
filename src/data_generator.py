65d20a9e26c1500948d4fdd4c77a8933731bbef2
nick doyle <nick.doyle09@gmail.com>
2023-09-22 18:15:56 -0400

    Adding in try/except errors

--- src/data_generator.py
+++ src/data_generator.py
@@ -19,36 +19,38 @@ team_names = {
     'Horizon': 7
 }
 
-num_districts = 50
-num_managers = 50
 
-# Generate num_districts of School objects
 def generate_school_districts(num_districts):
     # Source School Objects. Delete school and level.
-    schools_df = pd.DataFrame([fake.school_object() for _ in range(num_districts)])
-    schools_df.drop(['level', 'school'], inplace=True, axis=1)
+    schools = pd.DataFrame([fake.school_object() for _ in range(num_districts)])
+    schools.drop(['level', 'school'], inplace=True, axis=1)
 
     # Randomly assign Schools to a Project Team
-    schools_df['team_name'] = random.choices(list(team_names.keys()),
+    schools['team_name'] = random.choices(list(team_names.keys()),
                                           k=num_districts,
                                           weights=(list(team_names.values()))
                                              )
 
     # Properly case the District Name
-    schools_df['district'] = schools_df['district'].str.title()
-    print("\n 'schools_df' dataset generated")
+    schools['district'] = schools['district'].str.title()
 
     # Assign random project count
-    schools_df['project_count'] = schools_df.apply(
+    schools['project_count'] = schools.apply(
         lambda x: random.randint(1,3), axis=1
     )
 
-    # Return
-    return schools_df
-
     # # Save DataFrame to CSV
-    # schools.to_csv('data/Schools.csv')
-    # print("Dataset generated and saved as 'data/Schools.csv'")
+    try:
+        schools.to_csv('data/Schools.csv')
+        print("Dataset generated and saved as 'data/Schools.csv'")
+    except PermissionError:
+        print("PermissionError: Dataset not saved! Do you have project_data.csv open or lack permission to write?")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    # Return
+    return schools
+
 
 def generate_project_managers(num_managers):
     # Generate PM names
@@ -63,16 +65,21 @@ def generate_project_managers(num_managers):
                                            k=num_managers,
                                            weights=(list(team_names.values()))
                                            )
+    # # Save DataFrame to CSV
+    try:
+        pm_names.to_csv('data/Project_Managers.csv')
+        print("Dataset generated and saved as 'data/Project_Managers.csv'")
+    except PermissionError:
+        print("PermissionError: Dataset not saved! Do you have project_data.csv open or lack permission to write?")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
     # Return
     return pm_names
-    print("pm_names dataset generated")
-
-    # # Save DataFrame to CSV
-    # pm_names.to_csv('data/Project_Managers.csv')
-    # print("Dataset generated and saved as 'data/Project_Managers.csv'")
 
 
 def generate_supervisors(num_supervisors):
+
     # Generate PM names
     supervisor_first_names = [fake.unique.first_name() for _ in range(num_supervisors)]
     supervisor_last_names = [fake.unique.last_name() for _ in range(num_supervisors)]
@@ -84,13 +91,14 @@ def generate_supervisors(num_supervisors):
     # Assign Manager to a Project Team
     supervisor_names['team_name'] = list(team_names.values())
 
+    # Save DataFrame to CSV
+    try:
+        supervisor_names.to_csv('data/Supervisors.csv')
+        print("Dataset generated and saved as 'data/Supervisors.csv'")
+    except PermissionError:
+        print("PermissionError: Dataset not saved! Do you have project_data.csv open or lack permission to write?")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
     # Return
     return supervisor_names
-    print("supervisor_names dataset generated")
-
-    # # Save DataFrame to CSV
-    # mgr_names.to_csv('data/Managers.csv')
-    # print("Dataset generated and saved as 'data/Managers.csv'")
-
-
-print(generate_school_districts(20))

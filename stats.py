9bfb0910157824c74c2252b8e24adf559a90736e
nick doyle <nick.doyle09@gmail.com>
2023-09-22 18:18:28 -0400

    bye bye

--- stats.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-managers = pd.read_csv('data/Managers.csv')
-pms = pd.read_csv('data/Project_Managers.csv')
-projects = pd.read_csv('data/Projects.csv')
-schools = pd.read_csv('data/Schools.csv')
-
-print(schools.groupby('team_name').nces_id.count().reset_index())
